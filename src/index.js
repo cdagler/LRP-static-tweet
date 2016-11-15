7e76b9e1e9a34e2cb7558b15001e24d3e4865ad6
cdagler <cdagler@yahoo.com>
2016-11-15 05:12:57 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+var Tweet = React.createClass({
+  render: function() {
+    return (<div className="tweet">Tweet</div>)
+  }
+});
+
+ReactDOM.render(
+  <Tweet/>,
+  document.getElementById('root')
+);
