023b7b02c072b7415619ac35495387509cea5d80
cdagler <cdagler@yahoo.com>
2016-11-18 05:27:23 -0800

    Added Name and Message

--- src/index.js
+++ src/index.js
@@ -4,7 +4,46 @@ import './index.css';
 
 var Tweet = React.createClass({
   render: function() {
-    return (<div className="tweet">Tweet</div>)
+    return (
+      <div className="tweet">
+        <Avatar/>
+        <div className="content">
+          <NameWithHandle/>
+          <Message/>
+        </div>
+      </div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <img src="https://www.gravatar.com/avatar/cdagler"
+      className="avatar"
+      alt="avatar"/>
+    );
+  }
+});
+
+var Message = React.createClass({
+  render: function() {
+    return (
+      <div className="message">
+        This is less then 140 characters.
+      </div>
+    );
+  }
+});
+
+var NameWithHandle = React.createClass({
+  render: function() {
+    return (
+      <span className="name-with-handle">
+        <span className="name">Your Name</span>
+        <span className="handle">@yourhandle</span>
+      </span>
+    );
   }
 });
 
